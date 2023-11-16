8d3a6dfc56544cc7e0749491ef69f4322c67e45a
KaueMartins1924 <kaue.silvamartinsshopee@gmail.com>
2023-11-16 13:05:05 -0300

    exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let numeros = [15, 29, 17, 9]
+let strings = ["maçã", "batata", "pizza", "agua"]
+let ambos = [5, "tomate", true]
+console.log ("Array número:", numeros[3], "\nArray strings:", strings[0], "\nArray com número, strings e boleano:", ambos[2])
